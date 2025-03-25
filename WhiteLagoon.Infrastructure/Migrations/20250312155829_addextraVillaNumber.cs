e53152f988cea8b6ac7e0a0a5e8c32e876077fe5
Sivabala R <138695695+Siva27012@users.noreply.github.com>
2025-03-25 21:58:28 +0530

    Add files via upload

---
+++ WhiteLagoon.Infrastructure/Migrations/20250312155829_addextraVillaNumber.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addextraVillaNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 303);
+
+            migrationBuilder.InsertData(
+                table: "VillaNumbers",
+                columns: new[] { "Villa_Number", "SpecialDetails", "VillaId" },
+                values: new object[] { 104, null, 1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VillaNumbers",
+                keyColumn: "Villa_Number",
+                keyValue: 104);
+
+            migrationBuilder.InsertData(
+                table: "VillaNumbers",
+                columns: new[] { "Villa_Number", "SpecialDetails", "VillaId" },
+                values: new object[] { 303, null, 3 });
+        }
+    }
+}
